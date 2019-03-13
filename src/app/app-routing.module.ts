f2a9e97f40c2be209f182fefc904549bf75b7977
waqasfactor <waqasahmad883@gmail.com>
2019-03-13 15:47:38 +0500

    add some image in asset folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventListComponent } from './events/event-list.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'events', component: EventListComponent},
+  {path: 'events/:id', component: EventDetailsComponent},
+  {path: '', redirectTo: '/events', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

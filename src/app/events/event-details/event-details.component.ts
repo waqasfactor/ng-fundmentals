252dc132ceb3f9954a7a0499d176247de38026d6
waqasfactor <waqasahmad883@gmail.com>
2019-03-20 22:17:52 +0500

    create data model in project

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '../shared/event.model';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EventDetailsComponent implements OnInit {
 
-  event:any;
+  event:IEvent;
   constructor(private evetService: EventService, private route : ActivatedRoute) { }
 
   ngOnInit() {
--- src/app/events/event-thumnil/event-thumnil.component.ts
+++ src/app/events/event-thumnil/event-thumnil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output,  EventEmitter } from '@angular/core';
+import { IEvent } from '../shared/event.model';
 
 @Component({
   selector: 'event-thumnil',
@@ -6,7 +7,7 @@ import { Component, OnInit, Input, Output,  EventEmitter } from '@angular/core';
   styleUrls: ['./event-thumnil.component.css']
 })
 export class EventThumnilComponent implements OnInit {
-  @Input() event:any;
+  @Input() event:IEvent;
   @Output() eventClick = new EventEmitter();
   anyProperty:any = "some properties";
   constructor() { }
